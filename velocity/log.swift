2ed05391741ae13ffeb5f6452a09079a9f807a2e
Max Kofler <kofler.max.dev@gmail.com>
2023-06-04 18:00:17 +0200

    Updated log module with Loggable class
    The Loggable class is a convenience class one can extend for giving the
    log messages context. By extending this class and providing the init()
    function an appropriate context, this will provide all the VLog() calls
    with the context provided.

--- velocity/log.swift
+++ velocity/log.swift
@@ -16,13 +16,56 @@ enum Loglevel: Int {
     case Trace
 }
 
+/// A class mainly meant for extending to provide convenient access to context.
+/// This will hold an internal variable `context` that represents the current context.
+/// The best use of this class is by extending another class from this and calling `init()` to set the context correctly.
+class Loggable {
+    /// The current context in string form
+    var context: String;
+
+    init(context: String) {
+        self.context = context;
+    }
+
+    /// Log the specified message under the `Err` loglevel using the context of this class
+    /// - Parameter message: The message to print
+    func VErr(_ message: String) {
+        velocity.VErr(message, self.context);
+    }
+
+    /// Log the specified message under the `Err` loglevel using the context of this class
+    /// - Parameter message: The message to print
+    func VWarn(_ message: String) {
+        velocity.VWarn(message, self.context);
+    }
+
+    /// Log the specified message under the `Err` loglevel using the context of this class
+    /// - Parameter message: The message to print
+    func VInfo(_ message: String) {
+        velocity.VInfo(message, self.context);
+    }
+
+    /// Log the specified message under the `Err` loglevel using the context of this class
+    /// - Parameter message: The message to print
+    func VDebug(_ message: String) {
+        velocity.VDebug(message, self.context);
+    }
+
+    /// Log the specified message under the `Err` loglevel using the context of this class
+    /// - Parameter message: The message to print
+    func VTrace(_ message: String) {
+        velocity.VTrace(message, self.context);
+    }
+}
+
 /// The current loglevel to use
 var VLoglevel: Loglevel = Loglevel.Info;
 
 /// Log the specified message to the log
 /// - Parameter message: The message to print
+/// - Parameter context: The context to print for the message (optional)
 /// - Parameter level: The loglevel to use (optional) if `nil`, this will always print
-func VLog(_ message: String, level: Loglevel? = nil) {
+func VLog(_ message: String, _ context: String? = nil, level: Loglevel? = nil) {
     if let level = level {
         //Early return if the loglevel is too low
         if VLoglevel.rawValue < level.rawValue {
@@ -39,38 +82,53 @@ func VLog(_ message: String, level: Loglevel? = nil) {
         case Loglevel.Trace: c = "T"
         }
 
-        NSLog("[Velocity][\(c)] \(message)");
+        var prefix = "[Velocity][\(c)]";
+        if let context = context {
+            prefix += context;
+        }
+
+        NSLog("\(prefix) \(message)");
     } else {
-        NSLog("[Velocity] \(message)");
+        var prefix = "[Velocity][_]";
+        if let context = context {
+            prefix += context;
+        }
+
+        NSLog("\(prefix) \(message)");
     }
 }
 
 /// Log the specified message under the `Err` loglevel
 /// - Parameter message: The message to print
-func VErr(_ message: String) {
-    VLog(message, level: Loglevel.Err);
+/// - Parameter context: The context to print for the message (optional)
+func VErr(_ message: String, _ context: String? = nil) {
+    VLog(message, context, level: Loglevel.Err);
 }
 
 /// Log the specified message under the `Warn` loglevel
 /// - Parameter message: The message to print
-func VWarn(_ message: String) {
-    VLog(message, level: Loglevel.Warn);
+/// - Parameter context: The context to print for the message (optional)
+func VWarn(_ message: String, _ context: String? = nil) {
+    VLog(message, context, level: Loglevel.Warn);
 }
 
 /// Log the specified message under the `Info` loglevel
 /// - Parameter message: The message to print
-func VInfo(_ message: String) {
-    VLog(message, level: Loglevel.Info);
+/// - Parameter context: The context to print for the message (optional)
+func VInfo(_ message: String, _ context: String? = nil) {
+    VLog(message, context, level: Loglevel.Info);
 }
 
 /// Log the specified message under the `Debug` loglevel
 /// - Parameter message: The message to print
-func VDebug(_ message: String) {
-    VLog(message, level: Loglevel.Debug);
+/// - Parameter context: The context to print for the message (optional)
+func VDebug(_ message: String, _ context: String? = nil) {
+    VLog(message, context, level: Loglevel.Debug);
 }
 
 /// Log the specified message under the `Trace` loglevel
 /// - Parameter message: The message to print
-func VTrace(_ message: String) {
-    VLog(message, level: Loglevel.Trace);
+/// - Parameter context: The context to print for the message (optional)
+func VTrace(_ message: String, _ context: String? = nil) {
+    VLog(message, context, level: Loglevel.Trace);
 }
