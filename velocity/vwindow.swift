ec5dd9f21331b0088fc084571a7accb1347f361a
Max Kofler <kofler.max.dev@gmail.com>
2023-06-07 09:20:46 +0200

    Optimize RFB capturing
    By comparing the previous frame to the current frame, this allows
    Velocity to not send framebuffer updates to the client if the frame
    hasn't changed, reducing bandwith and CPU requirements.

--- velocity/vwindow.swift
+++ velocity/vwindow.swift
@@ -63,12 +63,28 @@ class VWindow : NSWindow {
                 return;
             }
 
-            guard let image = self.capture_window() else {
+            guard let new_image = self.capture_window() else {
                 return;
             }
 
-            for session in self.rfb_sessions {
-                try! session.frame_changed(frame: image);
+            do {
+                // If we have a previous image, check if it is the same
+                if let cur_image = self.cur_frame {
+                    if !cur_image.is_equal(to: new_image) {
+                        for session in self.rfb_sessions {
+                            try session.frame_changed(frame: new_image);
+                        }
+                        self.cur_frame = new_image;
+                    }
+                } else {
+                    for session in self.rfb_sessions {
+                        try session.frame_changed(frame: new_image);
+                    }
+                    self.cur_frame = new_image;
+                }
+
+            } catch {
+                VErr("Failed to update remote framebuffers");
             }
         }
     }
@@ -85,8 +101,35 @@ class VWindow : NSWindow {
             return nil
         }
 
-        self.cur_frame = cgImage;
-
         return cgImage;
+    }
+}
+
+extension CGImage {
+    /// Compares this image to another and returns if they are the same
+    /// - Parameter to: The image to compare this image to
+    func is_equal(to: CGImage) -> Bool {
+        // Compare dimensions
+        if self.width != to.width || self.height != to.height {
+            return false
+        }
+
+        // Compare raw pixel data
+        let dataSize = self.height * self.bytesPerRow
+        let data1 = UnsafeMutablePointer<UInt8>.allocate(capacity: dataSize)
+        let data2 = UnsafeMutablePointer<UInt8>.allocate(capacity: dataSize)
+        let colorSpace = CGColorSpaceCreateDeviceRGB()
+        let context1 = CGContext(data: data1, width: self.width, height: self.height, bitsPerComponent: self.bitsPerComponent, bytesPerRow: self.bytesPerRow, space: colorSpace, bitmapInfo: self.bitmapInfo.rawValue)
+        let context2 = CGContext(data: data2, width: to.width, height: to.height, bitsPerComponent: to.bitsPerComponent, bytesPerRow: to.bytesPerRow, space: colorSpace, bitmapInfo: to.bitmapInfo.rawValue)
+
+        context1?.draw(self, in: CGRect(x: 0, y: 0, width: self.width, height: self.height))
+        context2?.draw(to, in: CGRect(x: 0, y: 0, width: to.width, height: to.height))
+
+        let result = memcmp(data1, data2, dataSize) == 0
+
+        data1.deallocate()
+        data2.deallocate()
+
+        return result
     }
 }
