889b0cc0416992cd910f230e618cb83717c5bde5
Max Kofler <kofler.max.dev@gmail.com>
2023-07-25 18:30:54 +0200

    vDB: User and Group selection, altering
    This commit adds creation, selection and altering functions for users and groups

--- velocity/db/tables/vDBTUsers.swift
+++ velocity/db/tables/vDBTUsers.swift
@@ -10,6 +10,122 @@ import SQLite
 
 extension VDB {
 
+    /// A user in the database
+    /// > Warning: Altered member variables do not commit to the database unless `commit()` is called on the object
+    class User : Loggable {
+        /// The logging context
+        internal let context: String;
+        /// A reference to the database for later use
+        internal let db: VDB;
+
+        /// The unique user id
+        let uid: Int64;
+        /// The unique username
+        var username: String;
+        /// The password in hashed form
+        var pwhash: String;
+
+        /// Create a new User object.
+        /// > Warning: This will not create the user in the database, for this, one should call creating functions
+        init (db: VDB, username: String, pwhash: String, uid: Int64) {
+            self.context = "[vDB::User (\(username){\(uid)})]";
+            self.db = db;
+            self.username = username;
+            self.pwhash = pwhash;
+            self.uid = uid;
+        }
+
+        /// Provides an information string describing this user
+        func info() -> String {
+            return "User (uid: \(self.uid), username: '\(self.username)')";
+        }
+
+        /// Commits the current state of this user to the database
+        ///
+        /// The `uid` remains and is used as the primary key
+        func commit() throws {
+            let query = self.db.t_users.table.insert(or: .replace,
+                                                     self.db.t_users.uid <- self.uid,
+                                                     self.db.t_users.username <- self.username,
+                                                     self.db.t_users.password <- self.pwhash);
+            try self.db.db.run(query);
+        }
+
+        /// Removes the user from the database
+        func delete() throws {
+            let query = self.db.t_users.table.filter(self.db.t_users.uid == self.uid).delete();
+            try self.db.db.run(query);
+        }
+
+        /// Creates a new user in the database
+        /// - Parameter db: The database to use
+        /// - Parameter username: The new username to use
+        /// - Parameter password: The new password to use
+        /// - Parameter uid: (optional) If set, enforce a `uid` for the new user
+        static func create(db: VDB, username: String, password: String, uid: Int64? = nil) throws -> Swift.Result<User, Users.InsertError> {
+            let pwhash = VDB.hash_pw(password);
+            var n_uid: Int64 = 0;
+
+            switch try db.user_insert(username: username, pwhash: pwhash, uid: uid) {
+            case .success(let uid):
+                n_uid = uid;
+            case .failure(let e):
+                return Swift.Result.failure(e);
+            }
+
+            return Swift.Result.success(User(db: db, username: username, pwhash: pwhash, uid: n_uid));
+        }
+
+        /// Select a user from the database
+        /// - Parameter db: The database to use
+        /// - Parameter username: The `username` to search for
+        static func select(db: VDB, username: String) throws -> User? {
+            guard let row = try db.db.pluck(db.t_users.table.filter(db.t_users.username == username)) else {
+                return nil;
+            }
+
+            return User(db: db, username: row[db.t_users.username], pwhash: row[db.t_users.password], uid: row[db.t_users.uid]);
+        }
+
+        /// Select a user from the database
+        /// - Parameter db: The database to use
+        /// - Parameter uid: The `uid` to search for
+        static func select(db: VDB, uid: Int64) throws -> User? {
+            guard let row = try db.db.pluck(db.t_users.table.filter(db.t_users.uid == uid)) else {
+                return nil;
+            }
+
+            return User(db: db, username: row[db.t_users.username], pwhash: row[db.t_users.password], uid: row[db.t_users.uid]);
+        }
+
+        /// Ensures a user exists in the database. If the `uid` is not `nil`, this will search for an existing user with the provided `uid`, else the `username`.
+        /// If no matching user is found, this will create a new user and return it.
+        /// - Parameter db: The database to use
+        /// - Parameter username: The `username` to search for / use
+        /// - Parameter password: The `password` to use
+        /// - Parameter uid: (optional) The `uid` to search for / use
+        static func ensure(db: VDB, username: String, password: String, uid: Int64? = nil) throws -> Swift.Result<User, Users.InsertError> {
+            // If there is a uid, search for it
+            if let uid = uid {
+                // If the user has been found, return it
+                if let user = try User.select(db: db, uid: uid) {
+                    velocity.VTrace("Found user by uid (\(uid)): \(user.info())", "[vDB::User]");
+                    return Swift.Result.success(user);
+                }
+            } else {
+                // If the user has been found, return it
+                if let user = try User.select(db: db, username: username) {
+                    velocity.VTrace("Found user by username (\(username)): \(user.info())", "[vDB::User]");
+                    return Swift.Result.success(user);
+                }
+            }
+
+            // Else create a new user
+            velocity.VTrace("Creating new user (uid = \(String(describing: uid)), name = '\(username)')", "[vDB::User]");
+            return try Self.create(db: db, username: username, password: password, uid: uid);
+        }
+    }
+
     /// The `users` table
     struct Users : Loggable{
         let context = "[vDB::Users]";
@@ -50,7 +166,7 @@ extension VDB {
                 return Swift.Result.failure(.UsernameExists);
             }
 
-            var query = self.table.insert(self.username <- username, self.password <- password);
+            var query = self.table.insert(self.username <- username, self.password <- pwhash);
 
             // If a specific UID is requested
             if let uid = uid {
@@ -78,5 +194,40 @@ extension VDB {
     /// - Parameter uid: (optional) Desired UID
     func user_insert(username: String, pwhash: String, uid: Int64? = nil) throws -> Swift.Result<Int64, Users.InsertError> {
         return try self.t_users.insert(self.db, username: username, pwhash: pwhash, uid: uid);
+    }
+
+    /// Creates a new user in the database
+    /// - Parameter username: The new username to use
+    /// - Parameter password: The new password to use
+    /// - Parameter uid: (optional) If set, enforce a `uid` for the new user
+    func user_create(username: String, password: String, uid: Int64? = nil) throws -> Swift.Result<User, Users.InsertError> {
+        return try User.create(db: self, username: username, password: password, uid: uid);
+    }
+
+    /// Select a user from the database
+    /// - Parameter username: The `username` to search for
+    func user_select(username: String) throws -> User? {
+        return try User.select(db: self, username: username);
+    }
+
+    /// Select a user from the database
+    /// - Parameter uid: The `uid` to search for
+    func user_select(uid: Int64) throws -> User? {
+        return try User.select(db: self, uid: uid);
+    }
+
+    /// Ensures a user exists in the database. If the `uid` is not `nil`, this will search for an existing user with the provided `uid`, else the `username`.
+    /// If no matching user is found, this will create a new user and return it.
+    /// - Parameter username: The username to search for / use
+    /// - Parameter password: The password to use
+    /// - Parameter uid: (optional) The uid to search for / use
+    func user_ensure(username: String, password: String, uid: Int64? = nil) throws -> Swift.Result<User, Users.InsertError> {
+        return try User.ensure(db: self, username: username, password: password, uid: uid);
+    }
+
+    /// Hashes a password using a hashing algorithm
+    /// - Parameter pw: The password string to hash
+    static func hash_pw(_ pw: String) -> String {
+        return pw.sha256();
     }
 }
