4b3b01d94eb2a26257fbdd18b5208b1e7c4c1e77
Max Kofler <kofler.max.dev@gmail.com>
2023-08-23 08:55:13 +0200

    vAPI: /u/auth: Use RoutesBuilder

--- velocity/api/namespaces/vAPI_u/vAPI_u.swift
+++ velocity/api/namespaces/vAPI_u/vAPI_u.swift
@@ -40,7 +40,7 @@ extension VAPI {
         }
 
         // Register sub-endpoints
-        try self.register_endpoints_u_auth()
+        try self.register_endpoints_u_auth(route: route.grouped("auth"))
         try self.register_endpoints_u_user(route: route.grouped("user"))
     }
 }
