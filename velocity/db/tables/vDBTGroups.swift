ee8bcc81822cd37f089c3c73ef76432ede422049
Max Kofler <kofler.max.dev@gmail.com>
2023-08-29 20:41:01 +0200

    vDB: Group: Encodable, get_memberships(), list()
    The Group class is now an Encodable to allow for serialization.
    It implements the get_memberships() function for retrieving all users that have permissions on the group.
    It implements the list() function for retrieving all the groups on a database

--- velocity/db/tables/vDBTGroups.swift
+++ velocity/db/tables/vDBTGroups.swift
@@ -12,7 +12,7 @@ extension VDB {
 
     /// A group in the database
     /// > Warning: Altered member variables do not commit to the database unless `commit()` is called on the object
-    class Group : Loggable {
+    class Group : Loggable, Encodable {
         /// The logging context
         internal let context: String
         /// A reference to the database for later use
@@ -26,6 +26,23 @@ extension VDB {
         let parent_gid: Int64
         /// The group name (unique within the parent group)
         var name: String
+
+        /// The encodable keys
+        private enum CodingKeys : CodingKey {
+            case gid
+            case parent_gid
+            case name
+            case memberships
+        }
+
+        /// Provide encoding functionality
+        func encode(to encoder: Encoder) throws {
+            var container  = encoder.container(keyedBy: CodingKeys.self)
+            try container.encode(self.gid, forKey: .gid)
+            try container.encode(self.parent_gid, forKey: .parent_gid)
+            try container.encode(self.name, forKey: .name)
+            try container.encode(self.get_memberships(), forKey: .memberships)
+        }
 
         /// Create a new Group object.
         /// > Warning: This will not create the group in the database, for this, one should call creating functions
@@ -61,6 +78,41 @@ extension VDB {
         func delete() throws {
             let query = self.db.t_groups.table.filter(self.db.t_groups.gid == self.gid).delete()
             try self.db.db.run(query)
+        }
+
+        struct MembershipInfo : Encodable {
+            let uid: Int64
+            let name: String
+            let permissions: [Permission]
+        }
+
+        /// Return all memberships of this group
+        func get_memberships() throws -> [MembershipInfo] {
+            let t_m = self.db.t_memberships;
+
+            var memberships: [MembershipInfo] = []
+
+            // Iterate over all membership users
+            let query = t_m.table.filter(t_m.gid == self.gid).select(distinct: t_m.uid)
+            for row_m in try self.db.db.prepare(query) {
+                guard let user = try self.db.user_select(uid: row_m[t_m.uid]) else {
+                    continue
+                }
+
+                var permissions: [Permission] = []
+
+                // Iterate over all permissions on this group
+                let query = t_m.table.filter(t_m.gid == self.gid && t_m.uid == user.uid)
+                for permission in try self.db.db.prepare(query) {
+                    if let permission = try self.db.permission_select(pid: permission[t_m.pid]) {
+                        permissions.append(permission)
+                    }
+                }
+
+                memberships.append(MembershipInfo(uid: user.uid, name: self.name, permissions: permissions))
+            }
+
+            return memberships
         }
 
         /// Creates a new group in the database
@@ -128,6 +180,20 @@ extension VDB {
             // Else create a new group
             velocity.VTrace("Creating new group (gid = \(String(describing: gid)), parent_gid = \(parent_gid) name = '\(name)')", "[vDB::Group]")
             return try Self.create(db: db, name: name, parent_gid: parent_gid, gid: gid)
+        }
+
+        /// List all available groups
+        /// - Parameter db: The database to query and to store for later usage
+        static func list(db: VDB) throws -> [Group] {
+            var arr: [Group] = []
+            let tg = db.t_groups
+            let query = tg.table
+
+            for row in try db.db.prepare(query) {
+                arr.append(Group(db: db, name: row[tg.name], gid: row[tg.gid], parent_gid: row[tg.parent_gid]))
+            }
+
+            return arr
         }
     }
 
@@ -245,5 +311,10 @@ extension VDB {
     /// - Parameter gid: (optional) The `gid` to search for / use
     func group_ensure(name: String, parent_gid: Int64, gid: Int64? = nil) throws -> Swift.Result<Group, Groups.InsertError> {
         return try Group.ensure(db: self, name: name, parent_gid: parent_gid, gid: gid)
+    }
+
+    /// List back all groups available in this database
+    func group_list() throws -> [Group] {
+        return try Group.list(db: self)
     }
 }
