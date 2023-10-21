c93533e0f31cad54999c935c5d29765b2d219d5d
Max Kofler <kofler.max.dev@gmail.com>
2023-10-21 10:00:25 +0200

    /u/group - POST Fix wrong name
    Fixed an error where the /u/group - POST (groupinfo) response would insert the group name instead of the username in the 'memberships' section

--- velocity/db/tables/vDBTGroups.swift
+++ velocity/db/tables/vDBTGroups.swift
@@ -112,7 +112,7 @@ extension VDB {
                     }
                 }
 
-                memberships.append(MembershipInfo(uid: user.uid, name: self.name, permissions: permissions))
+                memberships.append(MembershipInfo(uid: user.uid, name: user.username, permissions: permissions))
             }
 
             return memberships
