c1da1ee23b0c8f7c5b44869fa47675a952905352
Max Kofler <kofler.max.dev@gmail.com>
2023-12-18 15:42:48 +0100

    VirtualMachineWindow: Add capture facilities
    This commit adds functionality for capturing the contents of the virtual machine screen and hold an internal copy of the last frame

--- velocity/virt/vmwindow.swift
+++ velocity/virt/vmwindow.swift
@@ -34,8 +34,16 @@ class VirtualMachineWindow : NSWindow, Loggable {
     /// The virtual machine view that this window holds and displays
     let vm_view: VZVirtualMachineView
 
+    /// The size of the screen. This is mainly for the RFB implementation to
+    /// have a quick and safe way to infer the screen size
+    let screen_size: NSSize
+
+    /// The last captured frame
+    var last_frame: CGImage? = nil
+
     init(vm_view: VZVirtualMachineView, size: NSSize) {
         self.vm_view = vm_view
+        self.screen_size = size
 
         // Create a style mask
         let transparent_window_style = NSWindow.StyleMask.init(rawValue: 0);
@@ -75,5 +83,40 @@ class VirtualMachineWindow : NSWindow, Loggable {
         // HACK: Set activation policy to .accessory to hide the window (dock...)
         NSApp.setActivationPolicy(.accessory)
         self.makeKeyAndOrderFront(nil)
+    }
+
+    /// Captures the current contents of this virtual machine window and updates
+    /// the internally held image
+    /// - Returns: If the capturing succeeded
+    func capture() -> Bool {
+        // Setup the screenshot capture
+        let windowListOption = CGWindowListOption(arrayLiteral: .optionIncludingWindow)
+        let imageOption: CGWindowImageOption = [.boundsIgnoreFraming, .nominalResolution]
+        let window_image = DispatchQueue.main.sync {
+            return CGWindowListCreateImage(.null, windowListOption, CGWindowID(self.windowNumber), imageOption)
+        }
+
+        // Check if the new image is valid
+        guard let new_image = window_image else {
+            return false
+        }
+
+        // Replace the old image, if it differs
+        if let old_image = self.last_frame {
+            if !old_image.is_equal(to: new_image) {
+                self.update_last_frame(new_image)
+            }
+        } else {
+            self.update_last_frame(new_image)
+        }
+
+        return true
+    }
+
+    /// Updates the internally held image of the last screenshot of the virtual machine
+    /// - Parameter frame: The frame to set
+    func update_last_frame(_ frame: CGImage) {
+        VTrace("Updating last frame")
+        self.last_frame = frame
     }
 }
