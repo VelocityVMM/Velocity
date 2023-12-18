993edd1bd757e32f710d59664e2cc3a011aa84a9
Max Kofler <kofler.max.dev@gmail.com>
2023-12-18 21:26:10 +0100

    VMManager: Implement capture engine
    This commit implements the capture engine for the virtual machine manager. It fires a timer in a specific interval to capture the current frames of all virtual machines.

--- velocity/manager/vmmanager.swift
+++ velocity/manager/vmmanager.swift
@@ -39,12 +39,20 @@ class VMManager : Loggable {
     /// The Database to use for all operations
     let db: VDB
 
+    /// The timer for the capture events
+    var capture_timer: Timer? = nil;
+    /// The amount of captures taken in one second
+    var capture_fps = 30;
+    /// The queue to do the capturing in
+    let capture_queue: DispatchQueue;
+
     /// Initializes the manager and tries to load all virtual machines
     /// - Parameter efistore_manager: The manager for all EFIStores
     /// - Parameter db: The database to use for all operations
     init(efistore_manager: EFIStoreManager, db: VDB) throws {
         self.efistore_manager = efistore_manager
         self.db = db
+        self.capture_queue = DispatchQueue(label: "Capture", attributes: .concurrent)
 
         // Iterate over all virtual machines available in the database
         for vm in try self.db.t_vms.get_all_vms(db: self.db) {
@@ -67,6 +75,9 @@ class VMManager : Loggable {
                 }
             }
         }
+
+        // Create the timer to fire in an interval for capture
+        self.capture_timer = Timer.scheduledTimer(timeInterval: 1.0 / Double(self.capture_fps), target: self, selector: #selector(self.update_frame), userInfo: nil, repeats: true);
     }
 
     /// Tries to retrieve a `VirtualMachine` by its `VMID`
@@ -74,5 +85,18 @@ class VMManager : Loggable {
     /// - Returns: The virtual machine if found, else `nil`
     func get_vm(vmid: VMID) -> VirtualMachine? {
         self.vms[vmid]
+    }
+
+    @objc func update_frame() {
+        // Iterate over all virtual machines
+        for vm in self.vms {
+            // If the virtual machine has an attached window
+            if let window = vm.value.window {
+                // Dispatch the work in an asynchronous dispatch queue to parallelize it
+                self.capture_queue.async {
+                    let _ = window.capture()
+                }
+            }
+        }
     }
 }
