4fe4f99cd574f161585c887f36df801adaa9e088
Max Kofler <kofler.max.dev@gmail.com>
2024-05-20 21:02:42 +0200

    Integrate rust code into xcode project

---
+++ Velocity/bridging-header.h
@@ -0,0 +1,7 @@
+#ifndef BridgingHeader_h
+#define BridgingHeader_h
+
+#include "./libvelocity/generated/SwiftBridgeCore.h"
+#include "./libvelocity/generated/libvelocity/libvelocity.h"
+
+#endif /* BridgingHeader_h */
